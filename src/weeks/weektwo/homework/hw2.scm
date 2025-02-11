46be4b1913746c522245f8b47b218dac531a2c07
Juan Maturino <juanjmaturino@gmail.com>
2025-02-11 16:48:55 -0600

    feature: finished exercise 1.40

--- src/weeks/weektwo/homework/hw2.scm
+++ src/weeks/weektwo/homework/hw2.scm
@@ -82,3 +82,65 @@
 
 ; relative prime product (rpp)
 (define (rpp num)(filtered-accumulate * 1 1 num identity increment (lambda(x)(isRelativePrime? num x))))
+
+
+; 1.40
+(define dx 0.00001)
+(define tolerance 0.001)
+(define (deriv g)
+  (lambda (x)
+    (/ (- (g (+ x dx)) (g x))
+       dx)))
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+	next
+	(try next)
+      )
+    )
+   )
+  (try first-guess)
+)
+
+
+(define (fixed-point-of-transform g transform guess)
+  (fixed-point (transform g) guess)
+)
+
+(define cube (lambda(x)(* x x x)))
+
+(define (newton-transform g)
+  (lambda (x)
+    (- x (/ (g x)
+	    ((deriv g) x)
+	 )
+    )
+   )
+)
+
+(define (newtons-method g guess)
+  (fixed-point-of-transform g newton-transform guess)
+)
+
+(define (cubic a b c)
+  (lambda(x)
+    (
+     + (cube x)
+     (* (sq x) a)
+     (* x b)
+     c
+     )    
+  )
+)
+
+
+
+; 1.41
+
+; 1.43
+
+; 1.46
