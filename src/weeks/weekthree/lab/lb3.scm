c18d40ba7d7f6af1e595f9e8ca07f98dbdcdb3c0
Juan Maturino <juanjmaturino@gmail.com>
2025-02-13 15:19:35 -0600

    feature: finished exercise 3 - changed helper function to accept a list instead of an integer as a paramter

---
+++ src/weeks/weekthree/lab/lb3.scm
@@ -0,0 +1,17 @@
+; # Exercise 3
+
+(define (count-change amount)
+  (cc amount '(50 25 10 5 1))
+)
+
+(define (cc amount kinds-of-coins)
+  (cond ((= amount 0)1)
+        ((or (< amount 0) (empty? kinds-of-coins)) 0)
+        (else
+          (+
+            (cc amount (bf kinds-of-coins))
+            (cc (- amount (first kinds-of-coins)) kinds-of-coins)
+          )
+        )
+  )
+)
