1a5768476499910353651f8b7a935a7d0bc319fa
Juan Maturino <juanjmaturino@gmail.com>
2025-03-11 15:48:06 -0500

    feature: added explaination of why the transformation x -> 1 + (1/x) can be used to calculate the golden ratio

--- src/weeks/weekthree/homework/p2.scm
+++ src/weeks/weekthree/homework/p2.scm
@@ -1,4 +1,11 @@
 ; Exercise 1.35
+; φ^2=φ+1. is an equation that is satisfied by the golden ratio
+; 
+; Observe that φ2=φ+1 can be rewriten to φ= φ + (1 / φ).
+; When this occurs, we can find the fixed point of the function in order to find a value
+; that satisfies the equation.
+;
+; By applying a fixed point procedure we verify that the golden ratio indeed, does satisfy the the equation
 (define tolerance 0.00001)
 
 (define (fixed-point f first-guess)
