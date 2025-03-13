acea83f1adab2d6f2fb11d586d11df6b9c7f74f0
Juan Maturino <juanjmaturino@gmail.com>
2025-03-12 21:40:29 -0500

    fix: after reviewing question, I saw that I had answered the first question incorrectly. I had made the procedure recursive, process iterative, but failed to acheive log(n) tc by applying sucessive squaring. Changed procedure to now iteratively apply sucessive squarring to find the value of a base (b) raised to exponent (n)

--- src/weeks/weekthree/homework/p1.scm
+++ src/weeks/weekthree/homework/p1.scm
@@ -1,5 +1,7 @@
 ; Exercise 1.16
 
+(define square (lambda(x) (* x x)))
+
 (define (fast-exp b e)
   (fast-exp-iter b e 1)
 )
@@ -7,6 +9,8 @@
 (define (fast-exp-iter base exponent a)
   (if (equal? exponent 0)
     a
-    (fast-exp-iter base (- exponent 1) (* a base)) ; tail call
+    (if (even? exponent)
+      (fast-exp-iter (square base) (/ exponent 2) a) ; tail call
+      (fast-exp-iter base (- exponent 1) (* base a)) ; tail call
   )
 )
