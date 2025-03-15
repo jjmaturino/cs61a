6a534255b977e0f6302611f05c15a1fa2c8ca8dc
Juan Maturino <juanjmaturino@gmail.com>
2025-03-14 20:05:55 -0500

    comment: added comment on why helper is outside scope of function using it

--- src/weeks/weekthree/homework/ee1.scm
+++ src/weeks/weekthree/homework/ee1.scm
@@ -2,6 +2,8 @@
   (helper n n 0)
 )
 
+; place outside for easier tracing... in practice would be useful to remove n parameter and access it via
+; the outer scope of the parent function it was defined in
 (define (helper n i currentSum)
     (cond ((equal? n currentSum) 1)
 	  ((or (< n currentSum) (equal? i 0)) 0)
