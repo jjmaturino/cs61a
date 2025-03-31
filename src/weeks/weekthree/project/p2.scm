d561bbb519454a1cb7588135c4f08aa0e7f4beeb
Juan Maturino <juanjmaturino@gmail.com>
2025-03-31 10:50:49 -0500

    fix: changed function signature to adhear with what the game function expects the api of a strategy to be

--- src/weeks/weekthree/project/p2.scm
+++ src/weeks/weekthree/project/p2.scm
@@ -3,17 +3,17 @@
 
 (define (test-stop-at-17)
   (and
-    (equal? (stop-at-17 '(ad 8s 5h)) #t)
-    (equal? (stop-at-17 '(ad 5h)) #t)
-    (equal? (stop-at-17 '(kd 3h 3s)) #t)
-    (equal? (stop-at-17 '(ad 4h 3s)) #f)
-    (equal? (stop-at-17 '(kd 4h 3s)) #f)
+    (equal? (stop-at-17 '(ad 8s 5h) 'ad) #t)
+    (equal? (stop-at-17 '(ad 5h) 'ad) #t)
+    (equal? (stop-at-17 '(kd 3h 3s) 'ad) #t)
+    (equal? (stop-at-17 '(ad 4h 3s) 'ad) #f)
+    (equal? (stop-at-17 '(kd 4h 3s) 'ad) #f)
   )
 )
 
-(define stop-at-17 (lambda (hand)
+(define stop-at-17 (lambda (playerhand dealercard)
     (let( 
-          (total (best-total hand)))
+          (total (best-total playerhand)))
       (
        if (< total 17) 
         #t
