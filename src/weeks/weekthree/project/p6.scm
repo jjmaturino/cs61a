f5f989087e1913814b38ec49d88e53b7e4da0187
Juan Maturino <juanjmaturino@gmail.com>
2025-04-01 12:10:32 -0500

    feature: finished part 6 of project 1 using composition of function

--- src/weeks/weekthree/project/p6.scm
+++ src/weeks/weekthree/project/p6.scm
@@ -1,4 +1,5 @@
 (load "project/p1")
+(load "project/p5")
 
 (define (test-suitmember)
   (and
@@ -16,5 +17,28 @@
     ((empty? hand) #f)
     ((equal? (getSuit (first hand)) suit) #t)
     (else (suitmember (bf hand) suit))
+  )
+)
+
+(define (test-valentine)
+  (and
+    (equal? #t (valentine '(ah 5d 2s) 'ad))
+    (equal? #t (valentine '(ah ad 5s) 'ad))
+    (equal? #t (valentine '(ad 5d) 'ad))
+    (equal? #t (valentine '(5d 5d 4s) 'ad))
+
+
+    
+    (equal? #f (valentine '(ad 5d 2s) 'ad))
+    (equal? #f (valentine '(ad ad 5s) 'ad))
+    (equal? #f (valentine '(ad 5d 2c) 'ad))
+    (equal? #f (valentine '(5d 5d 9s) 'ad))
+  )
+)
+
+(define (valentine hand card)
+  (if (suitmember hand 'h)
+    ((stop-at 19) hand card)
+    ((stop-at 17) hand card)
   )
 )
