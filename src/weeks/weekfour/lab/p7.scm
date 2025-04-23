b75bfcb5fccfaca1ce83265aa8b4faff3b2f0f3e
Juan Maturino <juanjmaturino@gmail.com>
2025-04-23 12:38:33 -0500

    feature: added method for calculating the length of a line segment

--- src/weeks/weekfour/lab/p7.scm
+++ src/weeks/weekfour/lab/p7.scm
@@ -3,6 +3,27 @@
 (define (make-segment p1 p2)(cons p1 p2))
 (define (start-segment s)(car s))
 (define (end-segment s)(cdr s))
+; added for exercise 2.3
+(define (segment-length s)
+  (sqrt
+    (+
+      (expt ( - (x-point (end-segment s)) (x-point (start-segment s))) 2)
+      (expt ( - (y-point (end-segment s)) (y-point (start-segment s))) 2)
+    )
+  )
+)
+
+(define test-segment-length(lambda()
+  (and
+    (equal? 4 (segment-length (make-segment (make-point 0 0) (make-point 4 0))))
+    (equal? 1 (segment-length (make-segment (make-point 0 4) (make-point 0 5))))
+    (equal? 4 (segment-length (make-segment (make-point 0 -2) (make-point 0 2))))
+  )
+ )
+)
+
+
+
 
 (define (make-point x y)(cons x y))
 (define (x-point p)(car p))
