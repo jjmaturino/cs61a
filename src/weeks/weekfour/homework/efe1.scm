2e91c409d1dbc1f5ba208385dc0364928bbfc873
Juan Maturino <juanjmaturino@gmail.com>
2025-05-02 14:36:46 -0500

    feature: finished first exercise for extra for experts

--- src/weeks/weekfour/homework/efe1.scm
+++ src/weeks/weekfour/homework/efe1.scm
@@ -1,1 +1,21 @@
 ; #TODO Extra for experts p1
+
+(define (helper cwd pl)
+    (let
+      ((curEl (first cwd))
+       (rest (bf cwd))
+      )
+      (if (equal? curEl 'r)
+        (if (equal? pl 'a)
+          (lambda(i)(car i))
+          (lambda(i)(cdr i))
+        )
+        (helper rest curEl)
+      )
+    )
+  )
+
+(define (cxr-function wd)
+
+  (helper wd "")
+)
