a3794e25d623b6da50b25f6b6dcb7a2a29a2acab
Juan Maturino <juanjmaturino@gmail.com>
2025-07-07 13:16:01 -0500

    fix: updated answer to be correct

--- src/weeks/weekfive/lab/p3.scm
+++ src/weeks/weekfive/lab/p3.scm
@@ -1,5 +1,9 @@
 ; (car ''abracadabra)
 
-; returns " ' "
+; returns " quote "
 ; because the string is represented as a list, where each character is car and the next substring is cdr.
 ; the first character after the string being initialized is " ' ", and thus when car is called, it returns the first character of the string
+
+; Since the quote expresion in scheme " ' ", which allows scheme to interact with symbols withouth evaluating their value,
+
+; Since ' is a special form, and the previous ' makes preceding symbols not be evaluated, when we encounter ' after calling car on the list of strings, we return the object representation of ', which is "quote"
