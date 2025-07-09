04ed619450639eed070d9088f99532bc1a714fa2
Juan Maturino <juanjmaturino@gmail.com>
2025-07-09 17:20:43 -0500

    feature: finished p2 of 2.29 SICP

--- src/weeks/weekfive/homework/p3.scm
+++ src/weeks/weekfive/homework/p3.scm
@@ -9,7 +9,7 @@
   (list length structure))
 
 
-;; Problem exercise
+;; p1: Selectors
 (define (left-branch mobile)
   (car mobile)
 )
@@ -69,5 +69,67 @@
       (equal? 7 (branch-structure (make-branch 57 7)))
       (equal? (make-branch 10 1) (branch-structure (make-branch 57 (make-branch 10 1))))
     )
+  )
+)
+
+; -- p2 : Total Weight of a mobile
+
+; Since a mobile is a type of tree structure, using lists to implement hierachical structuring, we can 
+; use list as interfaces to traverse the hierachy
+
+(define mobile1 (make-mobile 
+                  (make-branch 10 50) 
+                  (make-branch 14 50))
+)
+(define mobile2 (make-mobile 
+                  (make-branch 10 100) 
+                  (make-branch 1
+                      (make-mobile 
+                        (make-branch 14 50) 
+                        (make-branch 14 25))
+                  )
+                )
+)
+
+(define mobile3 (make-mobile 
+                  (make-branch 10 
+                               (make-mobile 
+                                 (make-branch 14 1) 
+                                 (make-branch 8 
+                                              (make-mobile 
+                                                (make-branch 14 8) 
+                                                (make-branch 14 7))))) 
+                  (make-branch 1 
+                               (make-mobile 
+                                 (make-branch 14 5) 
+                                 (make-branch 14 2)))))
+
+(define mobile4 (make-mobile 
+                  (make-branch 10 13) 
+                  (make-branch 10 14)))
+
+
+
+
+(define test-total-weight
+  (lambda()
+    (and
+      (equal? 100 (total-weight mobile1))
+      (equal? 175 (total-weight mobile2))
+      (equal? 23 (total-weight mobile3))
+      (equal? 27 (total-weight mobile4))
+    )
+  )
+)
+
+; we want to run total-weight on every sub-mobile
+; a mobile with no submobiles will return it's weight
+; a mobile has no submobiles  if it's left branch and right branch points to only weights
+
+(define (total-weight mobile)
+  (
+   if (number? (branch-structure mobile)) ; if branch structure is not a list (i.e a submobile)
+    (branch-structure mobile)
+    (reduce + (map total-weight (filter ( lambda(branch)(and (pair? branch) (not (equal? '() branch)))) mobile)))
   )
 )
