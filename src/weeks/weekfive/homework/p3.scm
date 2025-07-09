1fb275de63c8287d4a493540d26690f49ef234d8
Juan Maturino <juanjmaturino@gmail.com>
2025-07-09 16:19:20 -0500

    feature: p1.2 of exercise 2.29 SICP

--- src/weeks/weekfive/homework/p3.scm
+++ src/weeks/weekfive/homework/p3.scm
@@ -41,3 +41,33 @@
 
 
 
+(define (branch-length branch)
+  (car branch)
+)
+
+(define (branch-structure branch)
+  (cadr branch)
+)
+
+(define test-branch-length
+  (lambda ()
+    (and 
+      (equal? 10 (branch-length (make-branch 10 1)))
+      (equal? 1 (branch-length (make-branch 1 120)))
+      (equal? 57 (branch-length (make-branch 57 1)))
+      (equal? 57 (branch-length (make-branch 57 (make-branch 10 1))))
+    )
+  )
+)
+
+
+(define test-branch-structure
+  (lambda ()
+    (and 
+      (equal? 1 (branch-structure (make-branch 10 1)))
+      (equal? 120 (branch-structure (make-branch 1 120)))
+      (equal? 7 (branch-structure (make-branch 57 7)))
+      (equal? (make-branch 10 1) (branch-structure (make-branch 57 (make-branch 10 1))))
+    )
+  )
+)
