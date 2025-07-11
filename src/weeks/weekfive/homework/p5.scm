1ec45ed017758490252ad942ca7c1c00611e5bf1
Juan Maturino <juanjmaturino@gmail.com>
2025-07-11 17:15:13 -0500

    feature: finished problem 3.30 for SICP

--- src/weeks/weekfive/homework/p5.scm
+++ src/weeks/weekfive/homework/p5.scm
@@ -1,1 +1,21 @@
 ; 2.31
+
+(define (square x)(* x x))
+
+(define square-tree
+  (lambda(tree)
+    (tree-map square tree)
+  )
+)
+
+(define (tree-map prod tree)
+  (if (null? tree)
+    '()
+  (map 
+    (lambda (node)
+      (if (not (pair? node))
+        (prod node)
+        (tree-map prod node)))
+    tree
+  ))
+)
