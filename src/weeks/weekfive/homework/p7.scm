69258684874f5f20d833036b57258f277e64604b
Juan Maturino <juanjmaturino@gmail.com>
2025-07-14 14:51:41 -0500

    feature: finished exercise 3.36 SICP

--- src/weeks/weekfive/homework/p7.scm
+++ src/weeks/weekfive/homework/p7.scm
@@ -1,1 +1,15 @@
 ; 2.36
+
+; p1
+; (map (lambda(seq) (car seq)) seqs)
+
+; p2 
+; (map (lambda(seq) (cdr seq)) seqs)
+
+
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+      nil
+      (cons (accumulate op init (map (lambda(seq) (car seq)) seqs))
+            (accumulate-n op init (map (lambda(seq) (cdr seq)) seqs)))))
+
