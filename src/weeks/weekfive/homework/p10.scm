bbc5d9b0e65771bcc6db905995768429441acc4e
Juan Maturino <juanjmaturino@gmail.com>
2025-08-27 22:49:35 -0500

    feature: finished exercise 54

--- src/weeks/weekfive/homework/p10.scm
+++ src/weeks/weekfive/homework/p10.scm
@@ -1,1 +1,21 @@
-; 2.38
+; 2.54
+
+(define checkSymbol 
+  (lambda (x y)
+    (eq? x y)
+  )
+)
+
+
+
+
+
+(define (equalHW? x y)
+  (cond 
+    ((and (null? x) (null? y)) #t) ;; Base case: both empty lists
+    ((or (null? x) (null? y)) #f) ;; One empty, one not
+    ((or (not (list? x)) (not (list? y))) #f) ;; One is list, one is symbol  
+    ((and (not (list? x)) (not (list? y))) (eq? x y)) ;; Both are symbols 
+    (else (and (equalHW? (car x) (car y))
+               (equalHW? (cdr x) (cdr y))))))     ;; Both are lists: check first elements and rest
+
