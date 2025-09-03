c4fc2471696cab72753d5cc205ca6e2eaf8ebe28
Juan Maturino <juanjmaturino@gmail.com>
2025-09-03 18:10:37 -0500

    feature: finshed exercise 2.48 && 2.49

--- src/projects/projecttwo/picture.scm
+++ src/projects/projecttwo/picture.scm
@@ -197,22 +197,101 @@
 
 
 (define (make-frame origin edge1 edge2)
-  (cons origin (cons edge1 (cons edge2 '()))))
+  (cons origin (cons edge1 edge2)))
 
-(define (origin f)(car f))
-(define (edge1 f)(cadr f))
-(define (edge2 f)(cddr f))
+(define (origin-frame f)(car f))
+(define (edge1-frame f)(cadr f))
+(define (edge2-frame f)(cddr f))
 
 ;;
 ;; Exercise 2.48:
 ;;
+(define (make-segment v1 v2)(cons v1 v2))
+(define (start-segment s)(car s))
+(define (end-segment s)(cdr s))
+
+;;
 ;; Exercise 2.49:
+;;
+;; The painter that draws the outline of the designated frame.
+(define (frame-painter frame)
+  (let 
+    ((s1 (make-segment (origin-frame frame) (edge1-frame frame)))
+    (s2 (make-segment (edge1-frame frame) (make-vect 1 1)))
+    (s3 (make-segment (make-vect 1 1) (edge1-frame frame)))
+    (s4 (make-segment (edge1-frame frame) (origin-frame frame))))
+    (((segments->painter '(s1 s2 s3 s4)) frame))
+  )
+)
+
+(define (frame-outline-painter)
+  (let ((segments
+         (list (make-segment (make-vect 0 0) (make-vect 1 0))   ; bottom edge
+               (make-segment (make-vect 1 0) (make-vect 1 1))   ; right edge
+               (make-segment (make-vect 1 1) (make-vect 0 1))   ; top edge
+               (make-segment (make-vect 0 1) (make-vect 0 0))))) ; left edge
+    (segments->painter segments)))
+
+;; The painter that draws an “X” by connecting opposite corners of the frame.
+(define (x-frame-painter)
+  (let ((segments
+          (list (make-segment (make-vect 0 0) (make-vect 1 1))
+                (make-segment (make-vect 0 1) (make-vect 1 0)))))
+    (segments->painter segments))
+)
+
+;; The painter that draws a diamond shape by connecting the midpoints of the sides of the frame.
+(define (diamond-painter)
+  (let ((center (make-vect 0.5 0.5))
+        (half-diag-right (scale-vect 0.5 (make-vect 1 0)))
+        (half-diag-up (scale-vect 0.5 (make-vect 0 1))))
+    (let ((right-point (add-vect center half-diag-right))   ; (1, 0.5)
+          (top-point (add-vect center half-diag-up))        ; (0.5, 1)
+          (left-point (sub-vect center half-diag-right))    ; (0, 0.5)
+          (bottom-point (sub-vect center half-diag-up)))    ; (0.5, 0)
+      (let ((segments
+             (list (make-segment bottom-point right-point)    ; bottom-right
+                   (make-segment right-point top-point)       ; top-right
+                   (make-segment top-point left-point)        ; top-left
+                   (make-segment left-point bottom-point))))  ; bottom-left
+        (segments->painter segments))))
+)
+
+
+;;
+;; The wave painter.
+;;
+(define wave
+  (segments->painter
+   (list (make-segment (make-vect 0.4 1) (make-vect 0.35 0.85))
+	 (make-segment (make-vect 0.35 0.85) (make-vect 0.4 0.65))
+	 (make-segment (make-vect 0.4 0.65) (make-vect 0.3 0.65))
+	 (make-segment (make-vect 0.3 0.65) (make-vect 0.15 0.6))
+	 (make-segment (make-vect 0.15 0.6) (make-vect 0 0.85))
+	 (make-segment (make-vect 0 0.65) (make-vect 0.15 0.4))
+	 (make-segment (make-vect 0.15 0.4) (make-vect 0.3 0.6))
+	 (make-segment (make-vect 0.3 0.6) (make-vect 0.35 0.5))
+	 (make-segment (make-vect 0.35 0.5) (make-vect 0.25 0))
+	 (make-segment (make-vect 0.4 0) (make-vect 0.5 0.3))
+	 (make-segment (make-vect 0.5 0.3) (make-vect 0.6 0))
+	 (make-segment (make-vect 0.75 0) (make-vect 0.6 0.45))
+	 (make-segment (make-vect 0.6 0.45) (make-vect 1 0.15))
+	 (make-segment (make-vect 1 0.35) (make-vect 0.75 0.65))
+	 (make-segment (make-vect 0.75 0.65) (make-vect 0.6 0.65))
+	 (make-segment (make-vect 0.6 0.65) (make-vect 0.65 0.85))
+	 (make-segment (make-vect 0.65 0.85) (make-vect 0.6 1)))))
+
 ;;
 ;; Exercise 2.50:
 ;;
+
+;;
 ;; Exercise 2.51:
 ;;
+
+;;
 ;; Exercise 2.52:
+;;
 
 
 
