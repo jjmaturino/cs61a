a14178277ff164cc4ea46ac39213825f655e9aab
Juan Maturino <juanjmaturino@gmail.com>
2025-09-08 14:54:17 -0500

    refactor: provided a better answer than when I was being lazy

--- src/weeks/weeksix/lab/p1.scm
+++ src/weeks/weeksix/lab/p1.scm
@@ -1,9 +1,38 @@
+;;
+;; Tracing ((lambda (x) (+ x 3)) 5)
+
 ; It's a pair, so we go to the last case:
-; We evaluate the operator: its a lambda, which is self evaluating -> returns a lambda
-; We evaluate the operands: returns 5
+	;((pair? exp) (apply-1 (EVAL-1 (car exp))      ; eval the operator
+  ;	      (map eval-1 (cdr exp))))
 
-; We use apply to apply the operand to the operator
-; We call substitute to insert variable values for formal parameters.
-; Since (+ 5 3) is a list, we evaluate it
 
-; return 8
+  ; We need to evaulate the opeartor (lambda(x)(+ x 3))
+  ; (EVAL-1 (car expr))
+  
+  ; In eval, one , argument from last step (car expr) 
+  ; Triggers this predicate statement
+  ; 	((lambda-exp? exp) exp)
+  ; Thus returning the lambda function (lambda $(+ x 3))
+
+  ; We need to evalulate the operand (3)
+  ; (EVAL-1 (cadr expr))
+  
+  ; 3 is self evaluating/consitent expression; returns 3
+
+  ; we run apply-1 with the previously returned values.
+
+  ; this statement gets triggered
+  ;  " (eval-1 (substitute (caddr proc)
+  ;       (cadr proc)
+	;		     args
+	;		     '())))
+
+
+  ; Then substitute gets called recursively so that each formal paramter is the body of the lambda exp
+  ; is swapped with the values provided by the arguments to the function
+
+  ; apply gets the subsituted version and calls eval-1 with this value
+  ; which then gets passed to stk to be evaluated and applied
+  
+  ; return value 8
+
